0bc2751f817612c0e0744a81e443a34f3b17d28c
YuJeanCho <eugenie8@kakao.com>
2022-06-16 14:46:58 +0900

    [UPDATE] Add BOJ

---
+++ BOJ/3507-automatedTelephoneExchange.swift
@@ -0,0 +1,14 @@
+let n = Int(readLine()!)!
+var cnt = 0
+
+if n <= 198 {
+    for i in 1..<100 {
+        for j in 1..<100 {
+            if i + j == n {
+                cnt += 1
+            }
+        }
+    }
+}
+
+print(cnt)
