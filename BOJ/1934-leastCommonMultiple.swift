d487cf404083a5b8c84e80384e88bcca323dd75c
YuJeanCho <eugenie8@kakao.com>
2022-06-18 14:20:01 +0900

    [UPDATE] Add BOJ

---
+++ BOJ/1934-leastCommonMultiple.swift
@@ -0,0 +1,15 @@
+func gcd(_ a: Int, _ b: Int) -> Int {
+    let mod: Int = a % b
+    return 0 == mod ? min(a, b) : gcd(b, mod)
+}
+
+func lcm(_ a: Int, _ b: Int) -> Int {
+    return a * b / gcd(a, b)
+}
+
+let t = Int(readLine()!)!
+
+for _ in 0..<t {
+    let input = readLine()!.split(separator: " ").map{ Int($0)! }
+    print(lcm(input[0], input[1]))
+}
