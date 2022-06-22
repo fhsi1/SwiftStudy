3bda94acc957389785235f4101a6ab1c9afb4ad7
YuJeanCho <eugenie8@kakao.com>
2022-06-22 14:48:42 +0900

    [UPDATE] Add BOJ

---
+++ BOJ/7510-advancedMath.swift
@@ -0,0 +1,13 @@
+let n = Int(readLine()!)!
+
+for i in 1...n {
+    var input = readLine()!.split(separator: " ").map { Int($0)! }
+    input.sort(by: >)
+    print("Scenario #\(i):")
+    if input[0] * input[0] == input[1] * input[1] + input[2] * input[2] {
+        print("yes")
+    } else {
+        print("no")
+    }
+    print()
+}
